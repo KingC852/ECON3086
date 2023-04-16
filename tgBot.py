082c254472662d6b21f8c71fcfa306aaaf612262
KingC852 <88846778+KingC852@users.noreply.github.com>
2023-04-16 19:21:09 +0800

    Full function bot
    1. add about function
    2. finish search function
    3. finish prediction function 
    4. create home function
    5. fix update, search

--- tgBot.py
+++ tgBot.py
@@ -3,6 +3,8 @@ import telebot
 from telebot import types
 from consolidated_function import *
 import pandas as pd 
+pd.options.mode.chained_assignment = None
+
 
 
 BOT_TOKEN = '6120420567:AAHwSVOlFi333PdvhrFmV3Mai4lc0nGyaVY'
@@ -10,24 +12,34 @@ BOT_TOKEN = '6120420567:AAHwSVOlFi333PdvhrFmV3Mai4lc0nGyaVY'
 # Create an instance of the Telegram bot
 bot = telebot.TeleBot(BOT_TOKEN)
 video = open('output.mp4', 'rb')
-db_df = pd.read_csv('price_watch_20210405-20230405.csv')
+db_df = pd.read_csv('database2.csv')
 db_df["full_product_name"] = db_df["Brand"] + " " + db_df["Product Name"]
 unique_product_df = db_df.drop_duplicates(subset=['full_product_name', 'Product Code'])[["Category 1","Category 2","Category 3","Product Code","Brand","Product Name","full_product_name"]]
-today = dt.date.today()
+today = dt.date.today()-dt.timedelta(days=3)
+db_df = db_df[db_df['avg_unit_price'].apply(lambda x: isinstance(x, float))]
+db_df['avg_unit_price'] = db_df['avg_unit_price'].astype(float)
+about = """
+This code is for a Telegram bot that helps users make better purchasing decisions by providing insights into product pricing trends. The bot does this by analyzing historical price data of various products and offering recommendations based on price forecasts.
 
+The bot offers three main features:
+
+Product search & price prediction: Users can search for a specific product, and the bot will find the closest matches from a database of products. Upon selecting a product, the bot uses a special method to predict the product's price over the next week. Based on the prediction, it advises users whether to buy now or wait for a better price.
+
+Data update: The bot regularly updates its product database to provide the most accurate and up-to-date information.
+
+Bot termination: Users can choose to stop interacting with the bot, which will then send a goodbye video and end the session.)
+"""
 def generate_buttons(bts_names, markup):
     for button in bts_names:
         markup.add(types.KeyboardButton(button))
     return markup
 
-markup = generate_buttons(['Update','Product search', 'end bot'],
+markup = generate_buttons(["About",'Update','Product search', 'end bot'],
                           types.ReplyKeyboardMarkup(row_width=2))
+
 # Define a function that will handle the /start command
-
-
 @bot.message_handler(commands=['start'])
 def send_hello(message):
-    # print(db_df)
     message = bot.reply_to(message, """Hi there! What you want to do?""",
                            reply_markup=markup)
     
@@ -37,7 +49,6 @@ def send_hello(message):
 def home(message):
     message = bot.reply_to(message, """Hi there! What you want to do?""",
     reply_markup=markup)
-
     bot.register_next_step_handler(message, button)
 
 def search_product(message):
@@ -50,17 +61,17 @@ def search_product(message):
     bot.register_next_step_handler(message, preddict_price)
 
 def preddict_price(message):
-    # print(db_df)
-    # print(message.text)
     if message.text == 'None of the above':
         message = bot.reply_to(message, f'Sorry, I will send you back to home page', reply_markup=markup)
         home(message)
     else:
-        # print(db_df)
         one_year_ago = today - dt.timedelta(days=365)
-        product_df = product_query(db_df, str(today.strftime('%Y%m%d')),str(one_year_ago.strftime('%Y%m%d')), message.text)
-        print(product_df)
-        message = bot.reply_to(message, f'Predicted price is 1000', reply_markup=markup)
+        now = str(today.strftime('%Y%m%d'))
+        one_year = str(one_year_ago.strftime('%Y%m%d'))
+        result = forecast_price(message.text, db_df,int(now),int(one_year))
+        graph(message.text, db_df,int(now),int(one_year))
+        bot.send_photo(message.chat.id, photo=open('sample_plot.png', 'rb'))
+        message = bot.reply_to(message, f'{result}', reply_markup=markup)
         bot.register_next_step_handler(message, button)
 
 def kill_bot(message):
@@ -72,45 +83,54 @@ def kill_bot(message):
         bot.stop_polling()
 
     else:
-        message = bot.reply_to(message, """Hi there! What you want to do?""",
+        home(message)
+
+
+def button(message,df=db_df):
+    global db_df
+    try:
+        if message.text == 'Product search':
+            item = bot.reply_to(message, "What product do you want to search for?")
+            bot.register_next_step_handler(item, search_product)
+        
+        elif message.text == 'Update':
+            bot.reply_to(message, "Updating database, Please wait...")
+            date = str(today.strftime('%Y%m%d'))
+            db = initiate_update_db(df,int(20230412))
+            #drop the rows with average price is not float
+            db = db[db['avg_unit_price'].apply(lambda x: isinstance(x, float))]
+            db['avg_unit_price']  = db['avg_unit_price'].astype(float)
+            db['timestamp'] = db['timestamp'].astype(float)
+            #update the global database
+            db_df = db
+
+            message = bot.reply_to(message, 'Successful Updated', reply_markup=markup)
+            home(message)
+
+
+        elif message.text == 'end bot':
+            bot.send_video(message.chat.id, open(
+                'output.mp4', 'rb'), supports_streaming=True)
+
+            message = bot.reply_to(message, """Hi there! What you want to do?""",
                             reply_markup=markup)
         
-        bot.register_next_step_handler(message, button)
+            bot.register_next_step_handler(message, kill_bot)
+
+        elif message.text == 'About':
+                bot.reply_to(message,f"{about}")
+                home(message)
 
 
-def button(message,db_df=db_df):
-    
-    if message.text == 'Product search':
-        item = bot.reply_to(message, "What product do you wnat to search for?")
-        bot.register_next_step_handler(item, search_product)
-    
-    elif message.text == 'Update':
+        else:
 
-        # date = str(today.strftime('%Y%m%d'))
-        # print(db_df.columns.values)
-        db_df = initiate_update_db(db_df,int(20230412))
-        # print(db_df_update['timestamp'].tail(10))
-        message = bot.reply_to(message, 'Updated', reply_markup=markup)
-        message = bot.reply_to(message, """Hi there! What you want to do?""",
-        reply_markup=markup)
-        bot.register_next_step_handler(message, button)
-
-
-    elif message.text == 'end bot':
-        bot.send_video(message.chat.id, open(
-            'output.mp4', 'rb'), supports_streaming=True)
-
-        message = bot.reply_to(message, """Hi there! What you want to do?""",
-                               reply_markup=markup)
-        bot.register_next_step_handler(message, kill_bot)
-
-
-    else:
-
-        message = bot.reply_to(message, """Please Click the buttons""",
-                               reply_markup=markup)
-        bot.register_next_step_handler(message, button)
-
+            message = bot.reply_to(message, """Please Click the buttons""",
+                                reply_markup=markup)
+            bot.register_next_step_handler(message, button)
+    except:
+            message = bot.reply_to(message, """Please Click the buttons""",
+                                reply_markup=markup)
+            bot.register_next_step_handler(message, button) 
 
 # Start the bot
 
